3709bd5cd1fd8a9a67a18f17d7859d5d257e93f0
Elena Marinelli <emarinelli@server.fake>
2013-02-27 16:31:46 -0800

    First commit adds HelloTest.class, HelloTest.java, and README.txt to previously empty repo.

---
+++ JTest2_VEK/HelloTest.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class HelloTest{
+	public static void main(String[] args) {
+		System.out.println("Hello, Test!");
+		Random r = new Random();
+		System.out.println("Here is a pseudorandom number: " + r);
+	}
+}
