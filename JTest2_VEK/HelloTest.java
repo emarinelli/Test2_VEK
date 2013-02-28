dd8b9a35ec5957a3970e5e9983a2c91ffb0c8f67
Elena Marinelli <emarinelli@server.fake>
2013-02-27 16:46:45 -0800

    Second commit. Changed r to r.nextInt() & other edits.

--- JTest2_VEK/HelloTest.java
+++ JTest2_VEK/HelloTest.java
@@ -3,6 +3,8 @@ public class HelloTest{
 	public static void main(String[] args) {
 		System.out.println("Hello, Test!");
 		Random r = new Random();
-		System.out.println("Here is a pseudorandom number: " + r);
+		System.out.println("Here is one pseudorandom number: "  
+			+ r.nextInt());
+		System.out.println("Here are another two: " + "\n" + r.nextInt() 			+ "\n" + r.nextInt());
 	}
 }
